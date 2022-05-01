980f66bfad20aed2c7fd4242b5c68de54cf1cb3e
TiagoFialho02 <tiagofialho2002@gmail.com>
2022-05-01 22:16:33 +0100

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,17 @@
-urlHeader = "http://localhost:8080/";
+urlHeader = "https://97b99f16-22bb-410f-95cc-ceec57525e37-prod.e1-us-east-azure.choreoapis.dev/" + 
+            "dvpf/supermarketpurchasesapi/1.0.0";
 
 function getProducts() {
+
     products = [];
     productNameInput = document.getElementById("productNameInput")
     if (productNameInput.value != "") {
-        /**
-         * Recolhe todos os dados da base de dados para dentro dos vetores dos determinados objetos
-        */
+
         var products = [];
-        var url = urlHeader + "getItemsByName/" + productNameInput.value;
+        var url = urlHeader + "/brocadeAPI/getProductsByName?productName=" + productNameInput.value;
         var xhr = new XMLHttpRequest();
         xhr.responseType = "json";
+        xhr.setRequestHeader('API-Key', new Keys.API_KEY)
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
